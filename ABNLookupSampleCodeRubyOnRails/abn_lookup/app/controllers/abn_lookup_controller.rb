2772c65c7e31208252c8ce6c91c297df1d8310d9
ABN-SFLookupTechnicalSupport <43945280+ABN-SFLookupTechnicalSupport@users.noreply.github.com>
2018-10-11 13:57:55 +1100

    Added ABNLookup sample code

---
+++ ABNLookupSampleCodeRubyOnRails/abn_lookup/app/controllers/abn_lookup_controller.rb
@@ -0,0 +1,20 @@
+class AbnLookupController < ApplicationController
+  require "soap/wsdlDriver"
+  
+  def lookup
+    # Define the WSDL path
+    wsdl = "http://www.abn.business.gov.au/abrxmlsearch/ABRXMLSearch.asmx?WSDL"
+    
+    # All the methods avaliable are parsed and define based on the WSDL path
+    soap_client = SOAP::WSDLDriverFactory.new(wsdl).create_rpc_driver
+    
+    #Define the required options of the method
+    options = {:searchString => "#{params[:lookup][:abn]}", :includeHistoricalDetails => "n", :authenticationGuid => "#{params[:lookup][:guid]}"}
+    
+    #Result for use in view
+    @result = soap_client.ABRSearchByABN(options)
+  end
+  def index
+    
+  end
+end
---
+++ ABNLookupSampleCodeRubyOnRails/abn_lookup/app/helpers/abn_lookup_helper.rb
@@ -0,0 +1,2 @@
+module AbnLookupHelper
+end
